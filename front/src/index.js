20ce7838bfef3d7ef931139ca4afda9f09af40e8
est22 <anlia.seoul@gmail.com>
2021-10-11 03:22:03 +0900

    feat: add react-route

--- front/src/index.js
+++ front/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; // 라우터 적용
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+// App을 BrowserRouter로 감싸기
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter> 
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
