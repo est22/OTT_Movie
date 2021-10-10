2fecc85a5e6d5a22fbce88c0681c2f36355a2fc2
est22 <anlia.seoul@gmail.com>
2021-10-11 03:51:42 +0900

    feat: add nav bar

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
+import Navigation from './nav';
 
 const App = () => {
   return (
     <div>
+        <Navigation/>
       <ul>
         <li>
-          <Link to="/">홈</Link>
+          <Link to="/">라우팅 테스트</Link>
         </li>
 
       </ul>
