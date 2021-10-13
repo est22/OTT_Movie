d8f7a9318d0133466e8872d96fb51acf14657c15
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-13 18:38:35 +0900

    feat : models

--- back/models.py
+++ back/models.py
@@ -8,17 +8,25 @@ class Movie(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     movie_name = db.Column(db.String(255), nullable=False)  # 영화이름
-    release_year = db.Column(db.Date, nullable=False)  # 개봉연도
-    award_name = db.Column(db.String(255), nullable=False)  # 상이름
+    ranking = db.Column(db.Integer, nullable=False)  # 영화의 순위
     award_year = db.Column(db.Date, nullable=False)  # 수상연도
-    # 러닝타임(상영시간)
-    running_time = db.Column(db.String(255), nullable=False)
-    summary = db.Column(db.Text, nullable=False)  # 줄거리
-    rating = db.Column(db.Integer, nullable=False)  # 별점
-    director = db.Column(db.String(255), nullable=False)  # 감독
+    award_name = db.Column(db.String(255), nullable=False)  # 수상이름
+    release_year = db.Column(db.Date, nullable=False)  # 개봉연도
+    running_time = db.Column(db.String(255), nullable=False)  # 러닝타임(상영시간)
+    storyline = db.Column(db.Text, nullable=False)  # 영화의 줄거리
+    user_rating = db.Column(db.Integer, nullable=False)  # 유저평점
+    critic_rating = db.Column(db.Integer)  # 전문가평점
     img_link = db.Column(db.String(255), nullable=False)  # 포스터(이미지)
+    review_summary = db.Column(db.Text)  # imdb리뷰요약
+
     # movie:review = 1:n
-    reviews = db.relationship('Review', backref = 'review')
+    reviews = db.relationship('Review', backref='movie')
+    # movie:user = n:n connect
+    topics = db.relationship('User', secondary=movie_user,
+                             backref='movie')  # backref : 역참조
+    # movie:genre = n:n connect
+    topics = db.relationship('Genre', secondary=movie_genre, backref='movie')
+
 
 class User(db.Model):
     '''유저 테이블'''
@@ -33,9 +41,13 @@ class User(db.Model):
         self.password = password
 
 
-class Movie_User(db.Model):
-    '''영화-유저 중간자 테이블(favorite)'''
-    __tablename__ = 'user_movie'
+'''영화-유저 연결테이블(favorite)'''
+movie_user = db.Table('movie_user',
+                      db.Column('movie_id', db.Integer, db.ForeignKey(
+                          'movie.id'), primary_key=True),
+                      db.Column('user_id', db.Integer, db.ForeignKey(
+                          'user.id'), primary_key=True)
+                      )
 
 
 class Genre(db.Model):
@@ -44,19 +56,17 @@ class Genre(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     genre_name = db.Column(db.String(255))
+    movie = db.relationship(
+        'Movie', secondary=movie_genre, backref="movie_genre")
 
 
-class Movie_Genre(db.Model):
-    '''영화-장르 중간자 테이블'''
-    __tablename__ = 'user_movie'
-
-
-class Actor(db.Model):
-    '''배우 테이블'''
-    __tablename__ = 'keyword'
-
-    id = db.Column(db.Integer, primary_key=True)
-    actor_name = db.Column(db.String(255))
+'''영화-장르 연결테이블'''
+movie_genre = db.Table('movie_genre',
+                       db.Column('movie_id', db.Integer, db.ForeignKey(
+                           'movie.id'), primary_key=True),
+                       db.Column('genre_id', db.Integer, db.ForeignKey(
+                           'genre.id'), primary_key=True),
+                       )
 
 
 class Review(db.Model):
@@ -65,14 +75,18 @@ class Review(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(255), nullable=False)
-    content = db.Column(db.Text(), nullable=False)  # 댓글 내용
     write_time = db.Column(db.DateTime, default=datetime.utcnow())  # 작성 시간
+    content = db.Column(db.Text(), nullable=False)  # 댓글 내용
     rating = db.Column(db.Integer, nullable=False)  # 평가 별점
-    # movie_id = db.relationship('movie.id')
-    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
 
-    def __init__(self, user_name, movie_id):
+    # movie:review = 1:n (fk)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+
+    def __init__(self, user_name, write_time, content, rating, movie_id):
         self.user_name = user_name
+        self.write_time = write_time
+        self.content = content
+        self.rating = rating
         self.movie_id = movie_id
 
 
