c8f8b9c34d01675358a9a6dfa64495611f30e9ec
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-13 18:56:57 +0900

    feat:models

--- back/load_data.py
+++ back/load_data.py
@@ -12,11 +12,19 @@ with open('test_data.csv', 'r') as f:
     for row in reader:
         award_year = datetime.strptime(
             row['award_year'], '%Y-%m-%d').date()
+        # movie
         movie = Movie(
             id=int(row['id']),
             movie_name=row['movie_name'],
             award_name=row['award_name'],
             award_year=row['award_year'],
         )
+        # genre
+        movie = Movie(
+            id=int(row['id']),
+            genre_1=row['movie_name'],
+        )
         db.session.add(movie)
+    
+
     db.session.commit()
